5651ca4046aef5745d9b16de022b90c42256f06e
Esteban Echandia <esteban.echandia@mercadolibre.com>
2022-09-09 21:00:21 -0300

    1. Layout simularPlazoFijo 2. Actividad del boton de simular con intent a ConstituirPlazoFijo

---
+++ app/src/main/java/com/grupo/bancoutn/SimularPlazoFijo.java
@@ -0,0 +1,31 @@
+package com.grupo.bancoutn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class SimularPlazoFijo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonSimular = (Button)findViewById(R.id.botonSimular);
+
+        botonSimular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ConstituirPlazoFijo.class);
+                view.getContext().startActivity(intent);}
+        });
+
+    }
+
+
+
+}
