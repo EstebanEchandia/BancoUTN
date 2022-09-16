865dfa92d980dfeca265656ef200b9d8184e2f51
JuanFC25 <juankaprin@gmail.com>
2022-09-16 11:35:50 -0300

    avanzo en pantalla simular Plazo Fijo y cambio a jetpack

--- app/src/main/java/com/grupo/bancoutn/ConstituirPlazoFijo.java
+++ app/src/main/java/com/grupo/bancoutn/ConstituirPlazoFijo.java
@@ -2,13 +2,42 @@ package com.grupo.bancoutn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
+import com.grupo.bancoutn.databinding.ActivityConstituirPlazoFijoBinding;
+
 
 public class ConstituirPlazoFijo extends AppCompatActivity {
+
+    private ActivityConstituirPlazoFijoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_constituir_plazo_fijo);
+        binding = ActivityConstituirPlazoFijoBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Button botonSimular = binding.botonSimular;
+        Spinner moneda = binding.spinner;
+        final String[] opciones = {"PESOS","DOLARES","EUROS"};
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,opciones);
+        moneda.setAdapter(adapter);
+        //Button botonSimular = (Button)findViewById(R.id.botonSimular);
+
+        botonSimular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SimularPlazoFijo.class);
+                view.getContext().startActivity(intent);}
+        });
     }
+
+
+
 }
--- app/src/main/java/com/grupo/bancoutn/SimularPlazoFijo.java
+++ app/src/main/java/com/grupo/bancoutn/SimularPlazoFijo.java
@@ -2,29 +2,76 @@ package com.grupo.bancoutn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.widget.Button;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.grupo.bancoutn.databinding.ActivityConstituirPlazoFijoBinding;
+import com.grupo.bancoutn.databinding.ActivitySimularPlazoFijoBinding;
 
 public class SimularPlazoFijo extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private ActivitySimularPlazoFijoBinding binding;
 
-        Button botonSimular = (Button)findViewById(R.id.botonSimular);
+    private void acutalizarCampos (){
+        SeekBar barraDias = binding.seekBar;
+        TextView plazo = binding.plazo;
+        TextView capital = binding.capital;
+        TextView interesesGanados = binding.interesesGanados;
+        TextView montoTotal = binding.montoTotal;
+        TextView montoTotalAnual = binding.montoTotalAnual;
+        EditText montoCapital = binding.montoCapital;
 
-        botonSimular.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), ConstituirPlazoFijo.class);
-                view.getContext().startActivity(intent);}
-        });
+        plazo.setText("Plazo: " + barraDias.getProgress() + " días");
+        if(!montoCapital.getText().toString().equals("")) capital.setText("Capital: " + montoCapital.getText().toString());
+        if(!montoCapital.getText().toString().equals("")) interesesGanados.setText("Intereses ganados:"+ montoCapital.getText().toString());
+        if(!montoCapital.getText().toString().equals("")) montoTotal.setText("Monto total: $ idem anterior" + montoCapital.getText().toString());
+        if(!montoCapital.getText().toString().equals("")) montoTotalAnual.setText("Monto total Anual: $ idem anterior" + montoCapital.getText().toString());
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
 
+
+        super.onCreate(savedInstanceState);
+        binding = ActivitySimularPlazoFijoBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        TextView textoDias = binding.textoDias;
+        SeekBar barraDias = binding.seekBar;
+        barraDias.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textoDias.setText("Días: " + i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                acutalizarCampos();
+            }
+        });
+        Button calcular = binding.calcular;
+
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                acutalizarCampos();
+            }
+        });
+
+
+
+    }
 }
