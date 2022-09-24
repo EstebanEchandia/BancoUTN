f907f1d937c47c3dd77075db372b1332f0d4d223
eechandia <echandia.esteban@gmail.com>
2022-09-24 16:36:49 -0300

    Alert Dialog, boton constituir

--- app/src/main/java/com/grupo/bancoutn/ConstituirPlazoFijo.java
+++ app/src/main/java/com/grupo/bancoutn/ConstituirPlazoFijo.java
@@ -10,9 +10,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.grupo.bancoutn.databinding.ActivityConstituirPlazoFijoBinding;
+
+import java.util.Locale;
 
 
 public class ConstituirPlazoFijo extends AppCompatActivity {
@@ -20,40 +23,80 @@ public class ConstituirPlazoFijo extends AppCompatActivity {
     public static final int REQUEST_CODE = 1;
 
     private ActivityConstituirPlazoFijoBinding binding;
+    private String nombre;
+    private String apellido;
+    private String moneda;
+    private String capitalInicial;
+    private String plazoDias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //declaramos los binder
         binding = ActivityConstituirPlazoFijoBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
         Button botonSimular = binding.botonSimular;
-        Spinner moneda = binding.spinner;
+        Button botonConstituir = binding.botonConstituir;
+        Spinner spinnerMoneda = binding.spinner;
+        EditText editTextNombre = binding.textoNombre;
+        EditText editTextApellido = binding.textoApellido;
+
+        //Opciones del spinner
         final String[] opciones = {"PESOS","DOLARES","EUROS"};
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,opciones);
-        moneda.setAdapter(adapter);
-        //Button botonSimular = (Button)findViewById(R.id.botonSimular);
+        spinnerMoneda.setAdapter(adapter);
 
         botonSimular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //Guardamos los datos cargados por el usuario antes de pasar a la actividad
+                nombre = editTextNombre.getText().toString();
+                apellido = editTextApellido.getText().toString();
+                moneda = spinnerMoneda.getSelectedItem().toString();
+
+                //Debug
+                Log.e("nombre, apellido y moneda", nombre+' '+apellido+' '+moneda);
+
+
+                //Intent a actividad de simular plazo fijo, el startActivityForResult queda esperando una respuesta
                 Intent intent = new Intent(view.getContext(), SimularPlazoFijo.class);
                 startActivityForResult(intent, REQUEST_CODE);
             }
         });
+
+        botonConstituir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //popUp de dialogo constituido
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Felicitaciones "+nombre+" "+apellido+"!")
+                        .setMessage("Tu plazo fijo de "+capitalInicial+" "+moneda.toLowerCase()+" ha sido constituido!")
+                        .setPositiveButton("Joya!", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //no hacemos nada
+                            }
+                        })
+                        .show();
+            }
+        });
+
     }
 
+    //Este es el codigo que se ejecuta cuando vuelve de la actividad de SimularPlazoFijo
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         try {
             super.onActivityResult(requestCode, resultCode, data);
 
             if (requestCode == REQUEST_CODE  && resultCode  == RESULT_OK) {
-                String capitalInicial = data.getStringExtra("CapitalInicial");
-                String plazoDias = data.getStringExtra("PlazoDias");
-                Log.e("capital", capitalInicial);
-                Log.e("plazo", plazoDias);
+                capitalInicial = data.getStringExtra("CapitalInicial");
+                plazoDias = data.getStringExtra("PlazoDias");
+
+                //Debug
+                Log.e("capital y plazo", capitalInicial+' '+plazoDias);
 
             }
         } catch (Exception ex) {
